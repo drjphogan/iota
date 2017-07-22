b272a559e0d26895233b5a1e095ef51efb5e1bef
drjphogan <dr.j.p.hogan@gmail.com>
2017-07-22 16:37:09 +0100

    Add project files.

---
+++ iota/Source.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+using std::vector;
+
+int main()
+{
+	// first comment
+	vector<int> x(10);
+	std::iota(x.begin(), x.end(),0);
+	auto first_even = std::find_if(x.begin(), x.end(), [](const int& item) { return item % 2 == 0 && item != 0; });
+	auto number_of_evens = std::count_if(x.begin(), x.end(), [](const int& item) { return item % 2 == 0 && item != 0; });
+	return 0;
+}
