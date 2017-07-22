250780e888a14d17711e4c1831469d63ecab8622
drjphogan <dr.j.p.hogan@gmail.com>
2017-07-22 17:07:48 +0100

    Third comment added to feature branch

--- iota/Source.cpp
+++ iota/Source.cpp
@@ -9,6 +9,7 @@ int main()
 	// Add the first comment
 	// Add a second comment
 	// A a comment to the master branch
+	// Add a third comment
 	vector<int> x(10);
 	std::iota(x.begin(), x.end(),0);
 	auto first_even = std::find_if(x.begin(), x.end(), [](const int& item) { return item % 2 == 0 && item != 0; });
