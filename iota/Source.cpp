c32c0aab80f9a040e00139debef35e9c870cea8c
drjphogan <dr.j.p.hogan@gmail.com>
2017-07-22 16:49:57 +0100

    Second comment added to source to feature branch

--- iota/Source.cpp
+++ iota/Source.cpp
@@ -7,6 +7,7 @@ using std::vector;
 int main()
 {
 	// Add the first comment
+	// Add a second comment
 	vector<int> x(10);
 	std::iota(x.begin(), x.end(),0);
 	auto first_even = std::find_if(x.begin(), x.end(), [](const int& item) { return item % 2 == 0 && item != 0; });
