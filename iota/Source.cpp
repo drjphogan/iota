9c81167d05aa3869bad6491878dfdd02afdc206e
drjphogan <dr.j.p.hogan@gmail.com>
2018-10-27 11:35:56 +0100

    Add space

--- iota/Source.cpp
+++ iota/Source.cpp
@@ -14,7 +14,8 @@ int main()
 	// Add fourth commit to master again
 	// Add fifth commit to master
 	// ADD a sixth comment
-	// add another flippin comment
+	// add another flippin comment 
+
 	vector<int> x(10);
 	std::iota(x.begin(), x.end(),0);
 	auto first_even = std::find_if(x.begin(), x.end(), [](const int& item) { return item % 2 == 0 && item != 0; });
